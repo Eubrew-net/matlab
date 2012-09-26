fe631023488c683d6dd1a3c8344c3e61cdcb3081
iberonesia <iberonesia@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2012-09-26 15:58:37 +0000

    Removing duplicate fles

--- matlab/brewer/database/date/diaj.m
+++
@@ -1,8 +0,0 @@
-function [diaj,datev]=diaj(date)
-   if isstr(date)
-       date=datenum(date);
-   end;
-   date=date(:);  
-   datev=datevec(date);
-   diaj=fix(date)-datenum(datev(:,1),1,1)+1;
-   
--- matlab/brewer/database/date/diajul.m
+++
@@ -1,6 +0,0 @@
-function [diaj,datev]=diaj(date)
-   if isstr(date) date=datenum(date); end
-   
-   datev=datevec(date);
-   diaj=date-datenum(datev(:,1),1,1)+1;
-   
