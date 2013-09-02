4102c546369a2f38037c68d170d0a1dd44af0b3d
juanjo <juanjo@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2013-09-02 23:42:35 +0000

    More precise information when loading B files

--- matlab/calibration/btools/read_bdata.m
+++ matlab/calibration/btools/read_bdata.m
@@ -135,10 +135,18 @@ for dd=CALC_DAYS
          [a b c]=fileparts(brw_config_files{b_idx,2});
         if ~isempty(strcat(b,c))
          [o3,config_,sl_,hg_]=readb_ds_develop(bfile,brw_config_files(b_idx,1:2));
-         disp(['OK-> ',bfile,' ',brw_name{b_idx},' ozone obs day ',num2str(unique(diaj(o3.ozone_ds(:,1)))')]);
+         if ~isempty(o3.ozone_ds)
+            disp(['OK-> ',bfile,' ',brw_name{b_idx},' ozone obs day ',num2str(unique(diaj(o3.ozone_ds(:,1)))')]);
+         else
+            disp(['OK-> ',bfile,' ',brw_name{b_idx},' No ozone for day ',num2str(unique(diaj(sl_.sl_cr(:,1)))')]);
+         end
         else
          [o3,config_,sl_,hg_]=readb_ds_develop(bfile,brw_config_files{b_idx,1});
-         disp(['OK-> ',bfile,' ',brw_name{b_idx},' ozone obs day ',num2str(unique(diaj(o3.ozone_ds(:,1)))')]);
+         if ~isempty(o3.ozone_ds)
+            disp(['OK-> ',bfile,' ',brw_name{b_idx},' ozone obs day ',num2str(unique(diaj(o3.ozone_ds(:,1)))')]);
+         else
+            disp(['OK-> ',bfile,' ',brw_name{b_idx},' No ozone for day ',num2str(unique(diaj(sl_.sl_cr(:,1)))')]);
+         end
         end
       end
     catch
