78c9964e33285ed0b1d73447ac3aed663c45a899
juanjo <juanjo@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2014-03-05 09:39:41 +0000

    What happened?

--- matlab/developinf/straylight_model.m
+++ matlab/developinf/straylight_model.m
@@ -1,4 +1,4 @@
-function result=straylight_model(o3c,A1,A2,Cal)
+function result=straylight_model(data,A1,A2,Cal)
 % 
 % Volodya:
 % A*m [x-k(m*x)^s]-ETC-MS9=0 -> A*m*x-ETC - k*A*m*(m*x)^s =MS9
@@ -9,13 +9,15 @@ function result=straylight_model(o3c,A1,A2,Cal)
 % (ms9-osc*A1)= K*A1(osc)^s+1+ETCo  
 % [r,gof,c]=fit(ozone_slant,ms9-o3p,'power2','Robust','on');
 %  
- ms9_inst=o3c(:,21);% F corr., en su caso
- m_inst=o3c(:,16);% m from inst
- o3_ref=o3c(:,13);% O3 de la ref, 2 confg.
+ ms9_inst=data(:,21);% F corr., en su caso
+ m_inst=data(:,16);% m from inst
+ o3_ref=data(:,7);% O3 de la ref, 2 confg.
  o3p=o3_ref.*(10*m_inst*A1); 
  ozone_slant=o3_ref.*m_inst/1000;% osc
  etcs=ms9_inst-o3p;% 1P method
- [grp,m,s]=osc_group(.2:.100:1.800,[etcs ozone_slant]);
+ min_osc=floor(min(ozone_slant)*10)/10; max_osc=ceil(max(ozone_slant)*10)/10; step_osc=0.05;
+%  [grp,m,s]=osc_group(.2:0.05:1.800,[etcs ozone_slant]);
+ [grp,m,s]=osc_group(min_osc:step_osc:max_osc,[etcs ozone_slant]);
 
  %% ploteo
  f=figure; set(f,'tag',strcat('ETC_StrayLight',Cal.brw_str{Cal.n_inst}));  
@@ -25,7 +27,7 @@ function result=straylight_model(o3c,A1,A2,Cal)
  % modelamos el stray-light segun Y = a*x^b+c
  % cflibhelp power
  [r,gof,c]=fit(m(:,2),m(:,1),'power2','Robust','on','StartPoint',[1,3,A2]);
- plot(.3:.1:1.7,r(.3:.1:1.7),'r.-'); grid         
+ plot(.30:.1:max_osc,r(.3:.1:max_osc),'r.-'); grid         
   
  R=coeffvalues(r);     RI=confint(r);
  % K=R(1)/A1/10/100 
@@ -42,5 +44,5 @@ function result=straylight_model(o3c,A1,A2,Cal)
  
  %% Output
  result.stats=gof; result.stats.RI=RI;
- result.coeff=R;
+ result.coeff.R=R; result.coeff.ETC=A2;
        
