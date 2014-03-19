bd0acd3393eb1eb91a5ed0819eb03f03e815b55c
iberonesia <57214302+eubrewnet@users.noreply.github.com>
2014-03-19 09:54:58 +0000

    added location on function

--- calibration/btools/readb_bdata.m
+++ calibration/btools/readb_bdata.m
@@ -1,9 +1,13 @@
 %ozone->[dsum,ozone_sum,config,ozone_ds,sl,sl_cr,hg,bhg,missing]=read_bdata(brewer)
 % lee los ficheros del directorio bfiles
-function [ozone,log,missing]=readb_bdata(brewer,path,brw_config_files)
+function [ozone,log,missing]=readb_bdata(brewer,path,brw_config_files,loc)
 
 
 i=brewer;
+
+if nargin==4
+        loc=[];
+end
 
 dsum={};    ozone_sum={};  ozone_ds={}; ozone_raw0={};
 config={};  sl={};     sl_cr={};     hg={};     bhg={};
@@ -31,7 +35,7 @@ for ii=1:length(sname)
         % aux_conf={fullfile(bfile_p,brw_config_files{i,1}),fullfile(bfile_p,brw_config_files{i,1})};
         %[ o3,config_,sl_,hg_]=readb_ds_develop(bfile,aux_conf);
         lastwarn('');
-        [ o3,config_,sl_,hg_]=readb_ds_develop(bfile,brw_config_files(i,1:2));
+        [ o3,config_,sl_,hg_,location]=readb_ds_develop(bfile,brw_config_files(i,1:2));
         %disp(['OK-> ',brw_name{i}]);
         catch
             err=lasterror;
@@ -40,19 +44,26 @@ for ii=1:length(sname)
         end
         
         try
-            dsum=[dsum;o3.dsum];
-            ozone_sum=[ozone_sum;o3.ozone_s];
-            config=[config;config_];
-            ozone_ds=[ozone_ds;o3.ozone_ds];
-            ozone_raw0=[ozone_raw0;o3.ds_raw0];
-            sl=[sl;sl_.sls_c];       % first calibration     
-            sl_cr=[sl_cr;sl_.sls_cr]; %recalculated/second calibration
-            hg=[hg;hg_.hg];
-            bhg=[bhg;hg_.time_badhg];
+            if isempty(loc) || strmatch(loc,location_)
+              dsum=[dsum;o3.dsum];
+              ozone_sum=[ozone_sum;o3.ozone_s];
+              config=[config;config_];
+              ozone_ds=[ozone_ds;o3.ozone_ds];
+              ozone_raw0=[ozone_raw0;o3.ds_raw0];
+              sl=[sl;sl_.sls_c];       % first calibration     
+              sl_cr=[sl_cr;sl_.sls_cr]; %recalculated/second calibration
+              hg=[hg;hg_.hg];
+              bhg=[bhg;hg_.time_badhg];
             
-            missing(index_day)=0;
-            log{index_day}={'OK',bfile,ext,lastwarn};
-        catch
+             missing(index_day)=0;
+             log{index_day}={'OK',bfile,ext,lastwarn};
+            else
+             disp(sprintff('%s  Not in location %s (%s)',bfile,loc,location.str));
+             disp(locaton)
+             log{index_day}={'ERROR',bfile,ext,'Location'};
+            end    
+                
+                catch
             disp('Error assingnig variables');
             log{index_day}={'ERROR',bfile,ext,'Variables'};
         end
