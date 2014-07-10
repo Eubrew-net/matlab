eaf7e6b470dd2674e594eb23b43e5063202c2e86
iberonesia <57214302+eubrewnet@users.noreply.github.com>
2014-07-10 11:26:48 +0000

    wellcome back the old file

--- brewer/uv/write_interchange_brewer.m
+++ brewer/uv/write_interchange_brewer.m
@@ -1,39 +1,7 @@
-function write_interchange_brewer(uv,varargin) 
-arg = inputParser;   % Create an instance of the inputParser class
-arg.FunctionName='write_interchange_brewer';
-arg.addRequired('uv', @isstruct);
-arg.StructExpand = false;
-arg.addParamValue('date_range', [], @isfloat) % por defecto: no control de fechas
-try
-  arg.parse(uv,varargin{:});
-  mmv2struct(arg.Results); 
-  Args=arg.Results;
-  chk=1;
-catch
-  errval=lasterror;
-  disp(errval.message);
-end
-
-s=uv; 
-if ~isempty(date_range)
-    dir_cell=struct2cell(s); files=dir_cell(10,:);
-%     myfunc_clean=@(x)regexp(x, '^uv\D.\d*','ignorecase')';    
-    clean=@(x)~isempty(x); 
-    remove=find(cell2mat(cellfun(clean,files, 'UniformOutput', false))==0);
-    files(remove)=[];  s(remove)=[];
-    myfunc=@(x)sscanf(x,'%*2c%3d%2d.%*d')';    
-    A=cell2mat(cellfun(myfunc,files, 'UniformOutput', false)');
-    %                    Año    Dia
-    dates=datejuli(A(:,2),A(:,1));    
-    s(dates<date_range(1))=[];    dates(dates<date_range(1))=[];
-    if length(date_range)>1
-       s(dates>date_range(2))=[]; dates(dates>date_range(2))=[];
-    end
-end
-
-for i=1:length(s)
-    if ~isempty(s(i).date)
-        write_interchange(s(i));
+function write_interchange_brewer(uv) 
+for i=1:length(uv)
+    if ~isempty(uv(i).date)
+        write_interchange(uv(i));
     end
 end    
 
@@ -65,8 +33,9 @@ function write_interchange(uv)
             file_int=sprintf('%03d%02d%02dG.iz1',diajul,hora,min);
           elseif  uv.inst==183
             file_int=sprintf('%03d%02d%02dG.iz2',diajul,hora,min);
-          elseif  uv.inst==185
-            file_int=sprintf('%03d%02d%02dG.iz3',diajul,hora,min);
+          %elseif  uv.inst==185  %rename brewer files with instrument
+          %number
+          %  file_int=sprintf('%03d%02d%02dG.iz3',diajul,hora,min);
           else  
             file_int=sprintf('%03d%02d%02dG.%03d',diajul,hora,min,uv.inst)
           end 
@@ -86,13 +55,13 @@ function write_interchange(uv)
           %fprintf(f,'operators:Alberto_Redondas Virgilio_Carreno Carlos Torres');
           fprintf(f,'No cosine correction\r\n#11\r\n#12\r\n#13\r\n');
           fprintf(f,'lamda(nm) irradiancia (W/m2) time \r\n');
-          jk=find(~isnan(uv.uv(:,i)));
-          scan_int=[uv.l(jk,i)/10,uv.uv(jk,i),uv.time(jk,i)/60];
+          scan_int=[uv.l(:,i)/10,uv.uv(:,i),uv.time(:,i)/60];
           fprintf(f,'%5.1f %2.8E %8.5f \r\n',scan_int');
           
           fclose(f);
       else
           warnnig([file_int ,' can open for output ']);
-          end          
+          end
+          
       end
       
