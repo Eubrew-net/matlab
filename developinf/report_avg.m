b1cfeeeb4bff099b9be6abc0ce7f8c01c2e386c0
juanjo <javierlopezsolano@gmail.com>
2014-08-14 11:04:30 +0000

    Improving functionality

--- developinf/report_avg.m
+++ developinf/report_avg.m
@@ -1,16 +1,27 @@
 function [tabla_avg sl_data,dt_data,rs_data,ap_data]=report_avg(Cal,varargin)
 
-% function tabla_avg=a1(Cal,varargin)
+% function tabla_avg=report_avg(Cal,varargin)
 % 
 % Analisis de los ficheros AVG a partir de la funcion brw_avg_report
 % Promedios por eventos
 % 
 % INPUT
-% - Cal   : variable de definiciones (setup)
-% - grp   : Opcional (string). Por defecto promedios mensuales
-%           Valores implementados: 'events','month','week','month+events' (see getevents function)
-% - outlier_flag: Opcional (cellstring con flags de depuracion). Por defecto no depuracion
-%                 7 elementos posibles: sl, dt, rs, ap, hg, h2o, op
+% - Cal         : variable de definiciones (setup)
+% 
+% Opcional
+% - grp         : (String). Por defecto promedios mensuales
+%                  Valores implementados: 'events','month','week','month+events' (see getevents function)
+% 
+% - grp_custom  : (Struct). Eventos personalizados 
+%                  Estructura con los campos siguientes (see getevents function)
+%                  1) dates  : Fechas asociadas a los eventos definidos
+%                  2) labels : Etiquetas asociadas a los eventos definidos
+% 
+% - outlier_flag: (Cellstring con flags de depuracion). Por defecto no depuracion
+%                  7 elementos posibles: sl, dt, rs, ap, hg, h2o, op
+% - fpath       : (String). Path a los ficheros AVG. Por defecto, Cal.path_root
+% 
+% - date_range  : (Float). Periodo de analisis. Por defecto, Cal.Date.CALC_DAYS 
 % 
 % OUTPUT
 % - tabla_avg: Estructura con los siguientes campos:
@@ -25,7 +36,12 @@ function [tabla_avg sl_data,dt_data,rs_data,ap_data]=report_avg(Cal,varargin)
 %                           'RS0','std','RS1','std','RS2','std','RS3','std','RS4','std','RS5','std','RS6','std','N'
 % 
 % EXAMPLE:
-%          tabla_avg=a1(Cal,'grp','events','outlier_flag',{'','dt','','','','',''});
+%  Eventos predefinidos  : 
+%                  report_avg(Cal,'grp','events','outlier_flag',{'','dt','','','','',''});
+% 
+%  Eventos personalizados: 
+%                  events=struct('dates',datenum(2014,1,[1 200]),'labels',{'Bef.  ND change','After. ND change'}); 
+%                  report_avg(Cal,'grp_custom',events,'outlier_flag',{'','dt','','','','',''});
 % 
 
 %% Validacion de input's
@@ -33,8 +49,10 @@ arg = inputParser;   % Create an instance of the inputParser class
 arg.FunctionName='report_avg';
 
 arg.addRequired('Cal', @isstruct);
-arg.addParamValue('grp', 'month', @(x)any(strcmpi(x,{'events','month','week','month+events'}))); % por defecto
-arg.addParamValue('outlier_flag', {'','','','','','',''}, @iscell); % por defecto no depuracion
+
+arg.addParamValue('grp', '', @(x)any(strcmpi(x,{'events','month','week','month+events'})));
+arg.addParamValue('grp_custom', [], @isstruct);    
+arg.addParamValue('outlier_flag', {'','','','','','',''}, @iscell);
 arg.addParamValue('fpath', Cal.path_root, @ischar);    
 arg.addParamValue('date_range', Cal.Date.CALC_DAYS, @isfloat);    
 
@@ -55,7 +73,18 @@ aux=scan_join(scan_join(scan_join(scan_join(Cal.Date.CALC_DAYS',sl_data(:,[1 12 
 %% Tabla por eventos 
  lbl_avg={'R6','std','R5','std','N','DT high','std','DT low','std','N','HT','std','+5V','std','SL current','std','N',...
           'RS0','std','RS1','std','RS2','std','RS3','std','RS4','std','RS5','std','RS6','std','N'}; 
- event_info=getevents(Cal,'grp',arg.Results.grp,'period',arg.Results.date_range); data_tab=meanperiods(aux, event_info);
+ if isempty(arg.Results.grp)
+    event_info=arg.Results.grp_custom;
+ else
+    event_info=getevents(Cal,'grp',arg.Results.grp,'period',arg.Results.date_range);      
+ end
+ if isempty(event_info)
+    fprintf('\rDebes definir una variable de eventos valida (help report_avg)\n');
+    tabla_avg=NaN; sl_data=NaN; dt_data=NaN; rs_data=NaN; ap_data=NaN;
+    return
+ end
+ data_tab=meanperiods(aux, event_info);
+ 
  aux=NaN*ones(size(data_tab.m,1),33);
  aux(:,[1 2 4 7 9 12:2:16 19:2:31])=data_tab.m(:,[1:5 13:15 6:12]);
  aux(:,[3 5 8 10 13:2:17 20:2:32])=data_tab.std(:,[2:5 13:end 6:12]);
