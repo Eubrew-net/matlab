0590eefa582ceb827d4325ddb85e1b5cadd105eb
iberonesia <iberonesia@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2014-09-22 13:24:38 +0000

    introduced dz measurements TODO: recalculation missing
     %% dz re-calculation: Pendiente. TODO
        %o3.ozone_raw=[timeds(:,1:9),ds(:,2),ds_temp,ds(:,7:13),F,F];
        %o3.ozone_raw_legend={'date'	'hg'    'idx'   'sza'	'm2'	'm3'	'sza'	'saz'	'tst'  'flt'  'temp'...
        %                        'OS0'  'OS1'	'OS2'	'OS3'	'OS4'	'OS5'	'OS6' 	...  % cuentas brutas
        %                        'iS0'  'iS1'	'iS2'	'iS3'	'iS4'	'iS5'	'iS6' 	 ...  % count-rates recalculadas 1 (Rayleight uncorrected !!)
        %                        'fs0'	'fs1'	'fs2'	'fs3'	'fs4'	'fs5'	'fs6'	...  % count-rates recalculadas 2 (Rayleight uncorrected !!)
        %                        };

--- matlab/calibration/btools/readb_ds_develop.m
+++ matlab/calibration/btools/readb_ds_develop.m
@@ -27,6 +27,8 @@ function [o3,config,sl_data,hg_data,loc]=readb_ds_develop(bfile,config_file,spec
 
 dsum=[];ds=[];dss=[];timeds=[];timedss=[]; ds_aod=[];
 ds=[];dss=[];ndss=0;timedsum=[];timeds=[];
+dz=[]; dzs=[]; timedz=[]; timedzsum=[]; 
+
 sl=[];sls=[];nsl=0;nsls=0;timesls=[];timesl=[];
 TC=[];config=[];
 config_2=[];
@@ -53,6 +55,12 @@ fmt_icf=[
 ' %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %s %s'];
 fmtsl=[' sl %c %d %f %d %d %d %d %d %d %d %d %d %d rat %f %f %f %f'];
 fmtsl_old=[' sl %c %d %f %d %d %d %d %d %d %d %d %d rat %d %d %d %d'];
+
+%formats for reading the bfile
+fmtdz=' dz %c %d %f %d %d %d %d %d %d %d %d %d %d %d rat %f %f %f %f';
+fmtsum='summary %d:%d:%d %c%c%c %f/ %f %f %f %f %c%c %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f %f'; % summary format
+ 
+
 
 %Filter definition
 filter=[0,64,128,192,256];
@@ -89,6 +97,7 @@ datestr(datefich(1));
 
 l=mmstrtok(s,char(10));
 
+jdz=strmatch('dz',l);
 jds=strmatch('ds',l);
 jsum=strmatch('summary',l);
 jco=strmatch('co',l);
@@ -96,8 +105,6 @@ jum=strmatch('um',l);
 jhg=strmatch('hg',l);
 jhgscan=strmatch('hgscan',l);
 jsl=strmatch('sl',l);
-
-
 jsc=strmatch('sc',l);
 
 %measures=cell('ds','zs','uq','co');
@@ -181,7 +188,7 @@ end
      end
      
    try  
-        %do not work on new versions of matlab ¿?
+        %do not work on new versions of matlab ??
         hg(length(jhg_old)+1:end,:)=cell2mat(textscan(char(l(jhg))','hg %f:%f:%f %f %f %f %f %f %f',...
                             'delimiter',char(13),'multipleDelimsAsOne',1));   
    catch
@@ -201,7 +208,7 @@ end
 %  aux_date=datevec(datefich(1)); aux_date=repmat(aux_date(1:3),size(hg(1:3,:)',1),2); aux_date(:,4:6)=hg(1:3,:)';
 %  aux_date=datenum(aux_date); aux=NaN*ones(size(hg,1)+1,size(hg,2)); aux(1,:)=aux_date;
 %  ix_bad=diff(time_hg); hg(ix_bad<0,:)=[]; time_hg=hg(1,:)*60+hg(2,:)+hg(3,:)/60;
-  time_hg=hg(1,:)*60+hg(2,:)+hg(3,:)/60; %a minutos. Lo de sort es un APAÑO
+  time_hg=hg(1,:)*60+hg(2,:)+hg(3,:)/60; %a minutos. Lo de sort es un APA?O
   flaghg=abs(hg(5,:)-config(14))<2; % more than 2 steps change
   if size(hg,2)>1
         flag_hg=find(diff(flaghg)==-1);      
@@ -256,7 +263,7 @@ end
                       ds=[ds,ds_];
                       hora=ds_(3)/60/24;
                       %time ds
-                      %timeds=fecha matlab,linea del fichero,nï¿½de
+                      %timeds=fecha matlab,linea del fichero,n???de
                       %sumario +temperatura
                       timeds=[timeds;[fecha+hora,jds(ds_idx(ii)),size(dss,2)*10+ii]];
                   end
@@ -293,8 +300,9 @@ end
       end
   end
 
+%% DS  
 if ~isempty(dss)
-    %Time calculation
+    % Time calculation
     % hora a formato matlab ->sumarios
     hora=dss(1,:)*60+dss(2,:)+dss(3,:)/60;
 
@@ -336,7 +344,7 @@ if ~isempty(dss)
        timeds(tb_ds,2)=0;
     end
 
-%      % flag hg utilizamos el numero de linea (ï¿½?)
+%      % flag hg utilizamos el numero de linea (????)
 %      timeds(:,2)=zeros(size(timeds(:,2)));
 %      timedss(:,2)=zeros(size(timedss(:,2)));
 %      if ~isempty(tb_dsum)
@@ -359,7 +367,7 @@ if ~isempty(dss)
 
     ds=ds';dss=dss';
 
-    %%  salidas raw DS    
+%%  salidas raw DS    
       o3.dsum=[timedss(:,1:2),timedss(:,8)/60,timedss(:,4),...
         dss(:,9:11),dss(:,[14,22,30,21,29]),dss(:,[15,23,16,24,17,25,18,26,19,27,20,28])];
       o3.dsum_legend={'date';'hgflag';'tst ';'aimass';'sza';'airm';'temp';...
@@ -380,7 +388,7 @@ if ~isempty(dss)
                         'm3*pressure corr';'cy ';'F0 ';'F1 ';'F2 ';'F3 ';...
                         'F4 ';'F5 ';'F6 ';'ms4 ';'ms5 ';'ms6 ';'ms7 ';'MS8 ';'MS9 '};    
 
-    %% ds re-calculation
+%% ds re-calculation
     %%% Prueba de vectorizar 
     %     l(4,1000)=NaN;
     %     for i=1:1000
@@ -589,7 +597,113 @@ else
     sl_data.sl_cr=[];
 
 end
+%% DZ
+if ~isempty(jdz)
+dss=[];dsum=[];ndzs=0;
+   for i=1:length(jsum)
+      dsum=sscanf(l{jsum(i)},fmtsum);
+      type=char(dsum(12:13)');
+      month=char(dsum(4:7)');
+      fecha=datenum(sprintf(' %02d/%s/%02d',dsum(7),month,dsum(8)));
+      hora=dsum(1)/24+dsum(2)/24/60+dsum(3)/24/60/60;
+      
+      if strmatch('dz',type)
+          ndzs=ndzs+1;
+          dz_idx=find(jdz-jsum(i)<0 & jdz-jsum(i)>=-4);
+          jdzsum(ndzs)=jsum(i);
+          if length(dz_idx)==4
+              %timedssum fecha,indice
+              timedzsum=[timedzsum;[fecha+hora,jsum(i)]];
+              dzs=[dzs,dsum];
+              for ii=1:4
+                  ds_=NaN*ones(18,1); 
+                  ds_aux=sscanf(l{jdz(dz_idx(ii))},fmtdz);
+                  if size(ds_aux,1)==18
+                     ds_=ds_aux; 
+                  else
+                     ds_(1:14)=sscanf(l{jdz(dz_idx(ii))},' dz %c %d %f %d %d %d %d %d %d %d %d %d %d %d');                  
+                  end
+                  dz=[dz,ds_];
+                  hora=ds_(3)/60/24;
+                  timedz=[timedz;[fecha+hora,jdz(dz_idx(ii)),size(dzs,2)*10+ii]];
+              end
+          end
+      else
+      end
+  end
+  % ---------------------- Start DZ output ------------------------------------
+  %fi ~isempty(dzs)
+    % Time calculation: hora a formato matlab -> sumarios
+    hora=dzs(1,:)*60+dzs(2,:)+dzs(3,:)/60;
+    % idx_dz indice de la medida dz
+    idx_dz=fix(timedz(:,3)/10);
+    % asignamos la temperatura al ds tomada del sumario
+    dz_temp=dzs(11,idx_dz)';
+    %filter check
+    if any(dzs(14,idx_dz)-(dz(2,:)/64))   %% why ?
+       % to chek that the filter are 0 64....
+       unique(ds(2,:))
+       unique(dss(14,:))
+       disp('error in filter check');
+       ds(2,:)=64*dss(14,idx_ds);
+    end       
+    
+    % calculo de los angulos zenitales y masa optica sumarios
+      [szadzs,m2dzs,m3dzs]=brewersza(hora',fileinfo(2),fileinfo(3),lat,long);
+      [sza,saz,tst_dz,snoon,sunrise,sunset]=sun_pos(timedzsum(:,1),lat,-long);
+      timedzs=cat(2,timedzsum,[szadzs,m2dzs,m3dzs,sza,saz,tst_dz,snoon,sunrise,sunset]);
+     % calculos se sza y masa op medidas individuales
+      [szadz,m2dz,m3dz]=brewersza(dz(3,:)',fileinfo(2),fileinfo(3),lat,long);
+      [sza,saz,tst_dz,snoon,sunrise,sunset]=sun_pos(timedz(:,1),lat,-long);
+      timedz=[timedz,[szadz,m2dz,m3dz,sza,saz,tst_dz,snoon,sunrise,sunset]];
 
+    %HGFILTER
+    % -> falla si no hay ningun hg->
+        tb_dz=[];    tb_dzsum=[];
+        for ii=1:size(time_badhg,1)
+          tb_dzsum=[tb_dzsum,find((hora>time_badhg(ii,1)   & hora<time_badhg(ii,2)))];
+          tb_dz=[tb_dz,find((dz(3,:)>time_badhg(ii,1) & dz(3,:)<time_badhg(ii,2)))];
+        end
+    % flag hg utilizamos el numero de linea (????)
+       timedz(:,2)=ones(size(timedz(:,2)));
+       timedzs(:,2)=ones(size(timedzs(:,2)));
+      if ~isempty(tb_dzsum)
+         timedz(tb_dz,2)=0;
+         timedzs(tb_dzsum,2)=0;
+      end
+      dz=dz'; dzs=dzs';
+
+     %%  salidas raw DS      
+      o3.dzsum=[timedzs(:,1:2),timedzs(:,8)/60,timedzs(:,4),...
+       dzs(:,9:11),dzs(:,[14,22,30,21,29]),dzs(:,[15,23,16,24,17,25,18,26,19,27,20,28])];
+      o3.dzsum_legend={'date';'hgflag';'hora ';'??';'ang2';'airm';'temp';'filt';'ozo ';'std_ozo';'so2 ';'std_so2';...
+        'ms4 ';'std_ms4';'ms5 ';'std_ms5';'ms6 ';'std_ms6';'ms7 ';'std_ms7';'ms8 ';'std_ms8';'ms9 ';'std_ms9'};
+       % ( bad for dz) sustituimos s0 s1 de la medida por m2 m3
+       dz(:,4:5)=[m2dz,m3dz*pr/1013];
+       dz(:,3)=tst_dz;
+       MS9=dz(:,15)-0.5*dz(:,16)-1.7*dz(:,17); % o3 double ratio ==MS(9)
+       MS8=dz(:,14)-3.2*dz(:,17);              %:REM SO2 ratio MS(8)       
+          
+       o3.dz_raw0=[timedz(:,1:3),dz_temp,dz,MS8,MS9];
+       o3.dz_raw0_legend={'date';'hgflag';'ndz';'tmp';'fl1';'fl2';'tim';'m2 ';'m3*pressure corr';'cy ';...
+                        'F0 ';'F1 ';'F2 ';'F3 ';'F4 ';'F5 ';'F6 '; 'F7 ';'r1 ';'r2 ';'r3 ';'r4 ';'MS8 ';'MS9 '};    
+
+    %% dz re-calculation: Pendiente. TODO
+    %o3.ozone_raw=[timeds(:,1:9),ds(:,2),ds_temp,ds(:,7:13),F,F];
+    %o3.ozone_raw_legend={'date'	'hg'    'idx'   'sza'	'm2'	'm3'	'sza'	'saz'	'tst'  'flt'  'temp'...
+    %                        'OS0'  'OS1'	'OS2'	'OS3'	'OS4'	'OS5'	'OS6' 	...  % cuentas brutas
+    %                        'iS0'  'iS1'	'iS2'	'iS3'	'iS4'	'iS5'	'iS6' 	 ...  % count-rates recalculadas 1 (Rayleight uncorrected !!)
+    %                        'fs0'	'fs1'	'fs2'	'fs3'	'fs4'	'fs5'	'fs6'	...  % count-rates recalculadas 2 (Rayleight uncorrected !!)
+    %                        };
+
+else
+    %disp('(no dz measurements)');
+    o3.dzsum=[]; o3.dzsum_legend=[];
+    o3.dz_raw0=[]; o3.dz_raw0_legend=[];
+end
+
+
+%% SUN SCAN
 if ~isempty(jco)
     co=l(jco);
     jsc_aux=strfind(co,'sc:');
@@ -684,7 +798,7 @@ if ~isempty(jco)
                             j=find(hgscan(:,1)>time_end,1,'first');
                             hg_end=hgscan(j,:);
                             
-                            %aï¿½adir al avg---> completar
+                            %a???adir al avg---> completar
                             
                             if ~isempty(hg_end) && ~isempty(hg_start)
                                 sc_flag=hg_end(6)-hg_start(6);
@@ -960,7 +1074,7 @@ function RC=rayleigth_cor(F,P,M3,BE)
   % input data
   %  DS counts/second
   %  
-  % 1ï¿½ Rayleight correction
+  % 1??? Rayleight correction
   %  Si se proporcionan coeficientes se calculan si no se usa el standard
   %
   %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
