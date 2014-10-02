3ad7fedd97b38a4198d27f2ed7427d11d388051c
juanjo <javierlopezsolano@gmail.com>
2014-10-02 13:10:42 +0000

    Now it is posibble to process all available data (i.e., not events-averaging)

--- developinf/report_dispersion.m
+++ developinf/report_dispersion.m
@@ -16,6 +16,9 @@ function [tabla_dsp,dsp_quad,dsp_cubic]=report_dispersion(Cal,varargin)
 %                  1) dates  : Fechas asociadas a los eventos definidos
 %                  2) labels : Etiquetas asociadas a los eventos definidos
 % 
+% Si no hemos asignado una valor a ninguno de los parámetros anteriores, entonces no se promedia por eventos,
+% sino que se analizan todos los dsp's en date_range.
+% 
 % - fpath      : (String). Path a los ficheros dsp. Por defecto, Cal.path_root\DSP 
 % 
 % - date_range  : (Float). PERIODO de analisis. Por defecto, Cal.Date.CALC_DAYS 
@@ -36,12 +39,12 @@ function [tabla_dsp,dsp_quad,dsp_cubic]=report_dispersion(Cal,varargin)
 %
 %                           Los campos wl_# y fwhm_# se refieren a las diffs. quad-cubic
 % EXAMPLE:
-%  Eventos predefinidos  : 
+%  Predefined Events : 
 %        tabla_dsp=report_dispersion(Cal,'grp','month+events');
-%
-%  Eventos personalizados: 
-%                  events=struct('dates',datenum(2014,1,[1 200]),'labels',{{'uno','dos'}}); 
-%                  tabla_dsp=report_dispersion(Cal,'grp_custom',events);
+% 
+%  Custom Events     : 
+%        events=struct('dates',datenum(2014,1,[1 200]),'labels',{{'uno','dos'}}); 
+%        tabla_dsp=report_dispersion(Cal,'grp_custom',events);
 
 %% Validacion de input's
 arg = inputParser;   % Create an instance of the inputParser class
@@ -57,7 +60,7 @@ arg.addParamValue('date_range', Cal.Date.CALC_DAYS, @isfloat);
 arg.parse(Cal, varargin{:});
 
 %%
-O3W=[0.00 0.00 -1.00 0.50 2.20 -1.70];
+O3W=[0.00 0.00 -1.00 0.50 2.20 -1.70];% ozone weighting factors
 
 [dsp_quad dsp_cubic]=read_dsp(arg.Results.fpath,'brwid',Cal.brw_str{Cal.n_inst},...
             'configs',Cal.brw_config_files,'date_range',arg.Results.date_range([1 end]));
@@ -72,7 +75,11 @@ aux(:,15:16)=cat(1,-O3W*abs(dsp_quad(:,18:23))',-O3W*abs(dsp_cubic(:,18:23))')';
                 'fwhm_0','fwhm_2','fwhm_3','fwhm_4','fwhm_5','fwhm_6',...
                 'A1 quad.','std','A1 cubic','std','N'};
  if isempty(arg.Results.grp)
-    event_info=arg.Results.grp_custom;
+    if ~isempty(arg.Results.grp_custom)
+       event_info=arg.Results.grp_custom;
+    else
+       event_info=struct('dates',arg.Results.date_range,'labels',{cellstr(datestr(fix(arg.Results.date_range)))}); 
+    end
  else
     event_info=getevents(Cal,'grp',arg.Results.grp,'period',arg.Results.date_range);      
  end
@@ -81,9 +88,18 @@ aux(:,15:16)=cat(1,-O3W*abs(dsp_quad(:,18:23))',-O3W*abs(dsp_cubic(:,18:23))')';
     tabla_dsp=NaN;
     return
  end
- data_tab=meanperiods(aux, event_info);
+ 
+ if isempty(arg.Results.grp) && isempty(arg.Results.grp_custom)
+    fprintf('\rProcessing all available dsp''s (not averaging)\n');
 
- tabla_dsp.data=cat(2,data_tab.m(:,1:14),data_tab.m(:,15),data_tab.std(:,15),...
-                      data_tab.m(:,16),data_tab.std(:,16),data_tab.N(:,end));
- tabla_dsp.events=data_tab.evnts;  tabla_dsp.data_lbl=lbl_dsp;
+    idx=findm(event_info.dates,aux(:,1),.5);
+    tabla_dsp.data=cat(2,aux(:,1:15),NaN*ones(length(idx),1),...
+                         aux(:,16)  ,NaN*ones(length(idx),1),ones(length(idx),1));
+    tabla_dsp.events=event_info.labels(idx);   tabla_dsp.data_lbl=lbl_dsp;
+ else
+    data_tab=meanperiods(aux, event_info);
+    tabla_dsp.data=cat(2,data_tab.m(:,1:14),data_tab.m(:,15),data_tab.std(:,15),...
+                         data_tab.m(:,16),data_tab.std(:,16),data_tab.N(:,end));
+    tabla_dsp.events=data_tab.evnts;  tabla_dsp.data_lbl=lbl_dsp;
+ end 
 
