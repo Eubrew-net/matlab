e25954ff68badb071d0647f408a7abb888be4220
iberonesia <57214302+eubrewnet@users.noreply.github.com>
2017-04-26 18:57:58 +0000

    stray light powerfit1 method

--- developinf/straylight_model.m
+++ developinf/straylight_model.m
@@ -15,10 +15,23 @@ function result=straylight_model(data,A1,A2,Cal)
  o3p=o3_ref.*(10*m_inst*A1); 
  ozone_slant=o3_ref.*m_inst/1000;% osc
  etcs=ms9_inst-o3p;% 1P method
- min_osc=floor(min(ozone_slant)*10)/10; max_osc=ceil(max(ozone_slant)*10)/10; step_osc=0.05;
+ min_osc=floor(min(ozone_slant)*10)/10;
+ max_osc=ceil(max(ozone_slant)*10)/10; 
+ step_osc=0.05;
 %  [grp,m,s]=osc_group(.2:0.05:1.800,[etcs ozone_slant]);
  [grp,m,s]=osc_group(min_osc:step_osc:max_osc,[etcs ozone_slant]);
-
+%add cero point
+ m0=mean(m(m(:,2)<0.5,:));
+ 
+ m0(2)=0.01;
+ m1=m0;
+ m1(2)=0.1;
+ m2=m0;
+ m2(2)=0.2;
+ 
+ m=[m0;m1;m2;m];
+ s=[s(1,:);s(1,:);s(1,:);s];
+ 
  %% ploteo
  f=figure; set(f,'tag',strcat('ETC_StrayLight',Cal.brw_str{Cal.n_inst}));  
  plot(ozone_slant,etcs,'.'); hold on;
@@ -27,9 +40,14 @@ function result=straylight_model(data,A1,A2,Cal)
  % modelamos el stray-light segun Y = a*x^b+c
  % cflibhelp power
  [r,gof,c]=fit(m(:,2),m(:,1),'power2','Robust','on','StartPoint',[1,3,A2]);
- plot(.30:.1:max_osc,r(.3:.1:max_osc),'r.-'); grid         
-  
+ plot(.10:.1:max_osc,r(.1:.1:max_osc),'r.-'); grid         
+ [r1,gof1,c1]=fit(m(:,2),m(:,1)-A2,'power1','Robust','on','StartPoint',[1,3]);
+ plot(.10:.1:max_osc,r1(.1:.1:max_osc)+A2,'r.-'); grid         
+ 
+ 
  R=coeffvalues(r);     RI=confint(r);
+ R1=coeffvalues(r1);     RI1=confint(r1);
+ 
  % K=R(1)/A1/10/100 
  % s=R(2)-1;
  % ETC=R(3)  
@@ -45,4 +63,6 @@ function result=straylight_model(data,A1,A2,Cal)
  %% Output
  result.stats=gof; result.stats.RI=RI;
  result.coeff.R=R; result.coeff.ETC=A2;
+ result.stats1=gof1; result.stats1.RI=RI1;
+ result.coeff1.R=R1; result.coeff1.ETC1=A2;
        
