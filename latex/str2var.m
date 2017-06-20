5ca54ceae9901cf8d9836a37cc60aed57d603201
iberonesia <57214302+eubrewnet@users.noreply.github.com>
2017-06-20 16:25:08 +0000

    string to var conversion

---
+++ latex/str2var.m
@@ -0,0 +1,22 @@
+function [ str ] = strd2var( str )
+% replace special characters for latex 
+
+str=deblank(str);
+str=strrep(str,' ','_');
+str=strrep(str,'#','_');
+str=strrep(str,'&','_');
+str=strrep(str,'?','_');
+str=strrep(str,'%','_');
+str=strrep(str,'-','_');
+str=strrep(str,'.','_');
+str=strrep(str,'/','_');
+
+%\~{n}
+
+
+
+
+
+
+end
+
