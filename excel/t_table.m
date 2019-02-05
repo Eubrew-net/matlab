f550412a8b25384082d581765c892c26b7cb4a9e
aredondasm <aredondasm@aemet.es>
2019-02-05 14:57:55 +0000

    is not used ¿?

--- excel/t_table.m
+++ excel/t_table.m
@@ -1,6 +1,6 @@
 function [ output_table ] = t_table( input_table,n,var_colum )
 
-%UNTITLED Summary of this function goes here
+% transposing tables Summary of this function goes here
 %   Detailed explanation goes here
 if nargin==1
     n=1;
@@ -12,16 +12,24 @@ end
 if isempty(n) n=1; end
 if n==1 
   s=vartype('numeric');
-  output_array= table2array(input_table(:,s));
-  output_table = array2table(output_array');
-  output_table.Properties.RowNames = rowname(input_table(:,s).Properties.VariableNames(:,n:end));
-
+  if ~isempty(input_table(:,s))
+     output_array= table2array(input_table(:,s));
+     output_table = array2table(output_array');
+     output_table.Properties.RowNames = rowname(input_table(:,s).Properties.VariableNames(:,n:end));
+  else
+     s=vartype('cell');
+     output_array= table2cell(input_table(:,s));
+     output_table = cell2table(output_array');
+     output_table.Properties.RowNames = rowname(input_table(:,s).Properties.VariableNames(:,n:end));
+  end
 else
   output_array= table2array(input_table(:,n:end));
   output_table = array2table(output_array');
   output_table.Properties.RowNames = rowname(input_table.Properties.VariableNames(:,n:end));
 end
-output_table = array2table(output_array');
+
+
+
 output_table.Properties.RowNames = rowname(input_table(:,s).Properties.VariableNames(:,n:end));
 if ~isempty(input_table.Properties.RowNames)
   output_table.Properties.VariableNames = colname(input_table.Properties.RowNames);
