7eedd6495de758f2f89a016aebbb46d6923d7cfe
aredondasm <aredondasm@aemet.es>
2019-04-23 13:01:16 +0000

    handle displaytable arguments

--- excel/display_table.m
+++ excel/display_table.m
@@ -2,17 +2,21 @@ function [ tabla ] = display_table(data, colheadings, wid, fms, rowheadings, fid
 %displaytable([config_orig(1:52),config_def(1:52),config_ref],[Cal.brw_config_files(Cal.n_inst,1),Cal.brw_config_files(Cal.n_inst,2),fref],20,'',cellstr(leg))
 t=array2table(data);
 colHeaders=cellstr(colheadings);
-rowNames=cellstr(rowheadings);
+
 try
     t.Properties.VariableNames=varname(colHeaders);
 catch
     disp('varname error');
     t.Properties.VariableDescriptions=colHeaders;
 end
-try
+
+if nargin>4
+ rowNames=cellstr(rowheadings);   
+ try
      t.Properties.RowNames=varname(rowNames);
-catch
+ catch
      t.Row=rowNames;    
+ end
 end
 tabla=t;    
 end
