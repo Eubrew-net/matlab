12206def6e9b0f2ebb7b700b6b06c6f2e0e65e63
aredondas <aredondas@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2019-05-13 10:00:43 +0000

    correct for single input

--- excel/makeHtml_Table.m
+++ excel/makeHtml_Table.m
@@ -8,18 +8,19 @@ function [ tabla ] = makeHtml_Table(M, T, rowNames, colHeaders, colors, strPreci
 
 %function sOut = makeHtmlTable(M, T, rowNames, colHeaders, colors, strPrecision)
 t=array2table(M);
-
-try
-    t.Properties.VariableNames=varname(colHeaders);
-catch
-    disp('varname error');
-    t.Properties.VariableDescriptions=colHeaders;
+if nargin>1
+    try
+        t.Properties.VariableNames=varname(colHeaders);
+    catch
+        disp('varname error');
+        t.Properties.VariableDescriptions=colHeaders;
+    end
+    try
+        t.Properties.RowNames=varname(rowNames);
+    catch
+        t.Row=rowNames;
+    end
+    tabla=t;
+else
+    tabla=t;
 end
-try
-     t.Properties.RowNames=varname(rowNames);
-catch
-     t.Row=rowNames;    
-end
-tabla=t;    
-end
-
