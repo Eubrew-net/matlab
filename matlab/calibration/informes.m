15fa11b6b20159fe0d10f17ef165217b449c7a52
aredondas <aredondas@31095142-d6ec-47c1-bc64-2c8fdb89a385>
2019-07-03 14:25:37 +0000

    clean process

--- matlab/calibration/informes.m
+++ matlab/calibration/informes.m
@@ -1,5 +1,5 @@
 
-function log=informes(Cal,n)
+function info_log=informes(Cal,n)
 if nargin==2
     N=n;
 else
@@ -15,7 +15,7 @@ persistent in;
 persistent ixn;
 
 log=zeros(Cal.n_brw,4);
-for in=1 % :4
+for in=1 :4
   for ixn=N
      try
       % options_pub.outputDir=fullfile(pwd,'latex','158','html'); options_pub.showCode=true;
@@ -23,15 +23,15 @@ for in=1 % :4
    
       options_pub.outputDir=fullfile(Cal.path_root,'latex',Cal.brw_str{ixn},'html');
       options_pub.showCode=true;
-      publish(fullfile(sprintf('cal_report_%03d%s.m',Cal.brw(ixn),info{in})),options_pub);
-      web{ixn,in}= [sprintf('\r\nhttps://www.iberonesia.net/svn/aro2013/html/%03d/html/cal_report_%03da1.html\r\n',[Cal.brw(ixn),Cal.brw(ixn)])];
+      publish(sprintf('cal_report_%03d%s.m',Cal.brw(ixn),info{in}),options_pub);
+      web{ixn,in}= [sprintf('\r\nhttps://www.eubrewnet.org/svn/are2019/html/%03d/html/cal_report_%03da1.html\r\n',[Cal.brw(ixn),Cal.brw(ixn)])];
       disp(['OK ',Cal.brw_str{ixn},' ',info(in)]);
-       % log(ixn,in)=1;
+      info_log(ixn,in)=1;
        
      catch
        close all;  
        disp(['ERROR',Cal.brw_str{ixn},' ',info(in)])
-       log(ixn,in)=-1;
+       info_log(ixn,in)=-1;
      end
      
   end
